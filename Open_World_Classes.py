3bc9c95f3cfff4fcc37b45c0f0cf43eae126af9b
CommandLord <18055729+CommandLord@users.noreply.github.com>
2018-02-24 21:08:58 +0100

    Created Classes py

---
+++ Open_World_Classes.py
@@ -0,0 +1,10 @@
+
+class Game:
+
+    def __init__(self,name,server_id,world,playes):
+        self.name = name
+        self.server_id = server_id
+        self.world = world
+        self.players = playes
+
+    def 
